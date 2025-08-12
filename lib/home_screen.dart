c77dffc6423f97243bfa8ffdaf1471a8f9bb41ef
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-08-12 14:12:56 +0600

    project done1

---
+++ lib/home_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  void diceChange() {
+   setState(() {
+     leftDiceNumber = Random().nextInt(6) + 1;
+     rightDiceNumber = Random().nextInt(6) + 1;
+   });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Center(child: Text('data')),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  diceChange();
+                },
+                child: Image(
+                    image: AssetImage('images/dice$leftDiceNumber.png')),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: diceChange,
+                child: Image(
+                    image: AssetImage('images/dice$rightDiceNumber.png')),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
